165f9fd110f2c879805818c3b0883df74096ec00
Vaikehu <118215415+Vaikehu@users.noreply.github.com>
2022-11-23 09:01:51 -1000

    1 file modified

--- pages/Portfolio.tsx
+++ pages/Portfolio.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import About from '../sections/About';
+import Contact from '../sections/Contact';
+import Header from '../sections/Header';
 import Navbar from '../sections/Navbar';
+import Services from '../sections/Services';
+import Skills from '../sections/Skills';
 
 const Portfolio = () => (
   <div>
     <Navbar />
+    <About />
+    <Header />
+    <Skills />
+    <Services />
+    <Contact />
   </div>
 );
 
